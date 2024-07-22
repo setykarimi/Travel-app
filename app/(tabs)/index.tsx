b27fccf6a27b06f4475fad12759ee29ef3227cde
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-07-22 06:38:34 +0330

    Subject: Main page ctack, Decription: Edit for main page, Date: 7/22/2024

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,36 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Stack } from "expo-router";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, TouchableOpacity } from "react-native";
+import Colors from "../constants/Colors";
 
 export default function Page() {
   return (
-    <View style={styles.container}>
-      <Text>Page</Text>
-    </View>
+    <Stack.Screen
+      options={{
+        headerTransparent: true,
+        headerTitle: "",
+        headerLeft: () => (
+          <TouchableOpacity onPress={() => {}} style={{ marginLeft: 20 }}>
+            <Image
+              source={{
+                uri: "https://xsgames.co/randomusers/avatar.php?g=female",
+              }}
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: 10,
+              }}
+            />
+          </TouchableOpacity>
+        ),
+        headerRight: () => (
+          <TouchableOpacity onPress={() => {}} style={{ marginRight: 20 }}>
+            <Ionicons name="notifications" size={20} color={Colors.black} />
+          </TouchableOpacity>
+        ),
+      }}
+    />
   );
 }
 
