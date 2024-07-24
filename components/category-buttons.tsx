94ec260a65c0e5c6505ec7d7772ef2b62c215e85
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-07-24 06:45:00 +0330

    Subject: Category buttons, Decription: Create new folder for components (category button), Date: 7/24/2024

---
+++ components/category-buttons.tsx
@@ -0,0 +1,66 @@
+import Colors from "@/app/constants/Colors";
+import destinationCategories from "@/data/categories";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import React from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+export default function CategoryButtons() {
+  return (
+    <View>
+      <Text style={styles.title}>Categories</Text>
+      <ScrollView
+        horizontal
+        contentContainerStyle={{
+          gap: 20,
+          paddingVertical: 10,
+          marginBottom: 10,
+        }}
+      >
+        {destinationCategories.map((item, index) => (
+          <TouchableOpacity
+            key={index}
+            onPress={() => {}}
+            style={styles.categoryBtn}
+          >
+            <MaterialCommunityIcons
+              name={item.iconName as any}
+              size={20}
+              color={Colors.black}
+            />
+            <Text style={styles.categoryBtnTxt}>{item.title}</Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 22,
+    fontWeight: "700",
+    color: Colors.black,
+  },
+  categoryBtn: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: Colors.white,
+    paddingHorizontal: 16,
+    paddingVertical: 10,
+    borderRadius: 10,
+    shadowColor: "#333333",
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+  },
+  categoryBtnTxt: {
+    marginLeft: 5,
+    color: Colors.black,
+  },
+});
