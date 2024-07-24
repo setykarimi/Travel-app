6c227c275ac3629da1a93d837a88bffc4d562b99
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-07-24 06:45:30 +0330

    Subject: Category buttons, Decription: Create new folder for data (categories data), Date: 7/24/2024

---
+++ data/categories.ts
@@ -0,0 +1,40 @@
+const destinationCategories = [
+  {
+    title: "All",
+    iconName: "hiking",
+  },
+  {
+    title: "Beaches",
+    iconName: "beach",
+  },
+  {
+    title: "Mountains",
+    iconName: "terrain",
+  },
+  {
+    title: "Cities",
+    iconName: "city",
+  },
+  {
+    title: "Forest",
+    iconName: "tree",
+  },
+  {
+    title: "Historical Sites",
+    iconName: "castle",
+  },
+  {
+    title: "National Parks",
+    iconName: "pine-tree",
+  },
+  {
+    title: "Islands",
+    iconName: "island",
+  },
+  {
+    title: "Deserts",
+    iconName: "weather-sunny",
+  },
+];
+
+export default destinationCategories;
