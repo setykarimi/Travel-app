79ad2b3f2aad06a3646ae8af4fd19b9d5bccd27f
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-07-30 07:01:38 +0330

    Subject: Listing, Decription: Pass Listing data and create new flatList, Date: 7/30/2024

--- components/listing.tsx
+++ components/listing.tsx
@@ -1,6 +1,37 @@
+import { ListingType } from "@/types/listing-type";
 import React from "react";
-import { View } from "react-native";
+import {
+  FlatList,
+  Image,
+  ListRenderItem,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-export default function Listing() {
-  return <View></View>;
+interface Props {
+  listings: any;
+}
+
+export default function Listing({ listings }: Props) {
+  const renderItem: ListRenderItem<ListingType> = ({ item }) => {
+    return (
+      <TouchableOpacity>
+        <View>
+          <Image source={{ uri: item.image }} />
+          <Text>{item.name}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+  return (
+    <View>
+      <FlatList
+        data={listings}
+        renderItem={renderItem}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      />
+    </View>
+  );
 }
