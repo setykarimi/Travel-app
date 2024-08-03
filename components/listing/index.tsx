052966b591be55411a113955656caad5df134494
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-08-03 06:50:03 +0330

    Subject: Listing, Decription: Pass category to listing and add loading, Date: 8/3/2024

--- components/listing/index.tsx
+++ components/listing/index.tsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, View } from "react-native";
 import ListItem from "./list-item";
 
 interface Props {
   listings: any;
+  category: string;
 }
 
-export default function Listing({ listings }: Props) {
+export default function Listing({ listings, category }: Props) {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 200);
+  }, [category]);
+
   return (
     <View>
       <FlatList
-        data={listings}
+        data={loading ? [] : listings}
         renderItem={ListItem}
         horizontal
         showsHorizontalScrollIndicator={false}
