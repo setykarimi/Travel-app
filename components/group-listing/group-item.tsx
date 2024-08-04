e6632f169f55394337714925dbf7879002557671
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-08-04 06:56:18 +0330

    Subject: Group listing, Decription: Add group list data, Date: 8/4/2024

--- components/group-listing/group-item.tsx
+++ components/group-listing/group-item.tsx
@@ -1,9 +1,56 @@
+import Colors from "@/constants/Colors";
 import { GroupType } from "@/types/group-type";
+import { Ionicons } from "@expo/vector-icons";
 import React from "react";
-import { ListRenderItem, View } from "react-native";
+import { Image, ListRenderItem, StyleSheet, Text, View } from "react-native";
 
-const GroupItem: ListRenderItem<GroupType> = () => {
-  return <View></View>;
+const GroupItem: ListRenderItem<GroupType> = ({ item }) => {
+  return (
+    <View style={styles.item}>
+      <Image source={{ uri: item.image }} style={styles.image} />
+      <View>
+        <Text style={styles.itemTxt}>{item.name}</Text>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Ionicons name="star" size={20} color={Colors.primaryColor} />
+          <Text style={styles.itemRating}>{item.rating}</Text>
+          <Text style={styles.itemReview}>({item.reviews})</Text>
+        </View>
+      </View>
+    </View>
+  );
 };
 
 export default GroupItem;
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: Colors.white,
+    padding: 10,
+    borderRadius: 10,
+    marginRight: 20,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  image: {
+    width: 80,
+    height: 100,
+    borderRadius: 10,
+    marginRight: 10,
+  },
+  itemTxt: {
+    fontSize: 14,
+    fontWeight: "600",
+    color: Colors.black,
+    marginBottom: 8
+  },
+  itemRating:{
+    fontSize: 14,
+    fontWeight:'600',
+    color: Colors.black,
+    marginLeft: 5
+  },
+  itemReview:{
+    fontSize: 14,
+    color: '#999'
+  }
+});
