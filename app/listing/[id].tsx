5cc06daed578060a3571bc14ee01e6c4297b7b34
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-08-05 07:08:53 +0330

    Subject: Destination details, Decription: Add data for destinaition detail, Date: 8/5/2024

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import Colors from "@/constants/Colors";
+import listingData from "@/data/destinations.json";
+import { ListingType } from "@/types/listing-type";
+import { Feather } from "@expo/vector-icons";
+import { Stack, useLocalSearchParams } from "expo-router";
+import React from "react";
+import {
+  Dimensions,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+const { width } = Dimensions.get("window");
+const IMG_HEIGHT = 300;
 
 export default function ListingDetails() {
+  const { id } = useLocalSearchParams();
+  const listing: ListingType = (listingData as ListingType[]).find(
+    (item) => item.id === id
+  );
   return (
-    <div>ListingDetails</div>
-  )
+    <>
+      <Stack.Screen
+        options={{
+          headerTransparent: true,
+          headerTitle: "",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => {}}>
+              <View
+                style={{
+                  backgroundColor: Colors.white,
+                  padding: 6,
+                  borderRadius: 10,
+                }}
+              >
+                <Feather name="arrow-left" size={20} />
+              </View>
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <View>
+        <Image source={{ uri: listing.image }} style={styles.image} />
+      </View>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: width,
+    height: IMG_HEIGHT,
+  },
+});
