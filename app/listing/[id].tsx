014d8bbde3018c2c2e6b9d33f0cc58610187dfe2
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-08-06 07:03:40 +0330

    Subject: Destination details, Decription: Add new data for details, Date: 8/6/2024

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,13 +1,14 @@
 import Colors from "@/constants/Colors";
 import listingData from "@/data/destinations.json";
 import { ListingType } from "@/types/listing-type";
-import { Feather } from "@expo/vector-icons";
-import { Stack, useLocalSearchParams } from "expo-router";
+import { Feather, FontAwesome5, Ionicons } from "@expo/vector-icons";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
 import React from "react";
 import {
   Dimensions,
   Image,
   StyleSheet,
+  Text,
   TouchableOpacity,
   View,
 } from "react-native";
@@ -20,6 +21,9 @@ export default function ListingDetails() {
   const listing: ListingType = (listingData as ListingType[]).find(
     (item) => item.id === id
   );
+
+  const router = useRouter();
+
   return (
     <>
       <Stack.Screen
@@ -27,7 +31,13 @@ export default function ListingDetails() {
           headerTransparent: true,
           headerTitle: "",
           headerLeft: () => (
-            <TouchableOpacity onPress={() => {}}>
+            <TouchableOpacity
+              onPress={() => router.back()}
+              style={{
+                backgroundColor: "rgba(255,255,255,0.5)",
+                padding: 4,
+              }}
+            >
               <View
                 style={{
                   backgroundColor: Colors.white,
@@ -39,10 +49,40 @@ export default function ListingDetails() {
               </View>
             </TouchableOpacity>
           ),
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => {}}
+              style={{
+                backgroundColor: "rgba(255,255,255,0.5)",
+                padding: 4,
+              }}
+            >
+              <View
+                style={{
+                  backgroundColor: Colors.white,
+                  padding: 6,
+                  borderRadius: 10,
+                }}
+              >
+                <Ionicons name="bookmark-outline" size={20} />
+              </View>
+            </TouchableOpacity>
+          ),
         }}
       />
-      <View>
+      <View style={styles.container}>
         <Image source={{ uri: listing.image }} style={styles.image} />
+        <View style={styles.contentWrapper}>
+          <Text style={styles.listingName}>{listing.name}</Text>
+          <View style={styles.listingLocationWrapper}>
+            <FontAwesome5
+              name="map-marker-alt"
+              size={18}
+              color={Colors.primaryColor}
+            />
+            <Text style={styles.lisitingLocationTxt}>{listing.location}</Text>
+          </View>
+        </View>
       </View>
     </>
   );
@@ -53,4 +93,28 @@ const styles = StyleSheet.create({
     width: width,
     height: IMG_HEIGHT,
   },
+  container: {
+    flex:1,
+    backgroundColor: Colors.white
+  },
+  contentWrapper: {
+    padding:20
+  },
+  listingName:{
+    fontSize:24,
+    fontWeight:'500',
+    color: Colors.black,
+    letterSpacing:0.5
+  },
+  listingLocationWrapper:{
+    flexDirection: 'row',
+    marginTop:5,
+    marginBottom:10,
+    alignItems:'center'
+  },
+  lisitingLocationTxt:{
+    fontSize:14,
+    marginLeft:5,
+    color: Colors.black
+  }
 });
